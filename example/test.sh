c8d00bf6181e1bd7e12997a2d2c6e238bb9f8705
Sid Doshi <sidoshi510@gmail.com>
2018-03-20 15:40:39 +0530

    Test with yarn and npm

--- example/test.sh
+++ example/test.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
 cd example
+echo "Testing npm"
 npm i
-node index.js
+node index.js || exit 1
+
+rm -rf node_modules
+rm -rf package-lock.json
+yarn cache clean
+
+echo "Testing yarn"
+yarn
+node index.js || exit 1
+cd ..
