1b5155757a9654d20c3e843860c24fca204e633d
Sid Doshi <sidoshi510@gmail.com>
2018-04-24 17:36:29 +0530

    Test with packed npm version

--- example/test.sh
+++ example/test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+yarn build
+npm pack
+mv git-ssh-key*.tgz example/git-ssh-key.tgz
+
 cd example
+rm -rf node_modules
 echo "Testing npm"
 npm i
 node index.js || exit 1
+echo "npm test passed sucessfully"
 
 rm -rf node_modules
 rm -rf package-lock.json
@@ -13,4 +19,11 @@ echo "Testing yarn"
 yarn -v
 yarn
 node index.js || exit 1
+echo "yarn test passed sucessfully"
+
+rm -rf node_modules
+rm -rf package-lock.json
+rm -rf yarn.lock
+rm -rf git-ssh-key.tgz
+
 cd ..
